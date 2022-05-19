169551c7c6065ab7c492f2fc141985e2c857ffc1
Matt Decker <matt@Matts-MBP.lan>
2022-05-19 17:20:29 -0400

    test crashed

--- index.js
+++ index.js
@@ -44,7 +44,20 @@ class Airplane {
 */
 
 class Person {
-  
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+  eat(someFood) {
+    return this.stomach.length < 10 ? this.stomach.push(someFood) : NaN;
+  }
+  poop() {
+    this.stomach = [];
+  }
+  toString() {
+    return `${this.name}, ${this.age}`
+  }
 }
 
 /*
@@ -62,7 +75,22 @@ class Person {
 */
 
 class Car {
-  
+  constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons) {
+    this.tank += gallons;
+  }
+  drive(distance) {
+    this.odometer += distance;
+    this.tank -= distance / this.milesPerGallon;
+  if (this.tank === 0) {
+    return `I ran out of fuel at ${this.odometer}, miles!`
+  }
+  }
 }
 
 /*
